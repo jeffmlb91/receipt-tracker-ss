176d3be868c81a7d092c72a67d86dac4a07cddb3
jeffmlb91 <66559059+jeffmlb91@users.noreply.github.com>
2025-03-20 23:41:22 -0400

    yodate to the page.tsx

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,13 +1,31 @@
 "use client";
 
 import React from 'react'
-import { Button } from './ui/button'
+import { Button } from './ui/button';
+import { Shield } from 'lucide-react'
+import Link from 'next/link';
+import { SignedIn, SignedOut, SignInButton, UserButton } from '@clerk/clerk-react';
+
 
 function Header() {
   return (
-    <div className="">
-        <h1>Hello World</h1>
-        <Button>Hello</Button>
+    <div className="p-4 flex justify-between items-center">
+        <Link href="/" className='flex items-center'>
+            <Shield className='w-6 h-6 text-blue-600 mr-2' />
+            <h1 className="text-l font-semibold">Expensio</h1>
+        </Link> 
+
+        <div className='flex items-center space-x-4'>
+          <SignedIn>
+            <UserButton/ >
+          </SignedIn>
+          
+          <SignedOut>
+             <SignInButton mode="modal">
+                <Button>Login</Button>
+             </SignInButton>
+          </SignedOut>
+        </div>
     </div>
   )
 }
